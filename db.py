4d5c8022f7c8023e006a0174cbd07485438c7ddc
bxu <buote.xu@gmail.com>
2017-12-24 23:43:45 +0100

    More views

--- db.py
+++ db.py
@@ -13,7 +13,10 @@ def remake_db():
 
         with con: # auto-commits
                 with con.cursor() as cur:
-
+                    cur.execute('''DROP VIEW IF EXISTS all_joined_view''')
+                    cur.execute('''DROP VIEW IF EXISTS all_view''')
+                    cur.execute('''DROP VIEW IF EXISTS concordance_composition_joined_view''')
+                    cur.execute('''DROP VIEW IF EXISTS concordance_composition_view''')
                     cur.execute('''DROP VIEW IF EXISTS composition_composer_view''')
                     cur.execute('''DROP TABLE IF EXISTS composition_composer''')
                     cur.execute('''DROP TABLE IF EXISTS composition_concordance_entry''')
@@ -45,13 +48,53 @@ def remake_db():
                                                                                   concordance_index integer CHECK (concordance_index BETWEEN 0 AND 2 )
                                                                                     )''')
                     cur.execute('''CREATE VIEW composition_composer_view as
-                    SELECT c1.name as composer_src, c2.name as composer_rism, c3.name as composer_chr, c.track_no, c.title, c.remark from
+                    SELECT c1.name as composer_src, c2.name as composer_rism, c3.name as composer_chr, 
+                    c.track_no, c.title, c.remark, c.mode, c.printed_in, c.genre, c.scribe, c.id as composition_id from
                     composition c join composition_composer cc on c.id = cc.composition
                     LEFT JOIN composer c1 on cc.composer_src = c1.id
                     LEFT JOIN composer c2 on cc.composer_rism = c2.id
                     LEFT JOIN composer c3 on cc.composer_chr = c3.id
                     ;
                     ''')
+
+                    cur.execute('''CREATE VIEW concordance_composition_view as
+                                SELECT concordance.shorthand as concordance_man, NULL as concordance_print, NULL as concordance_chr, composition_id
+                                from composition_concordance_entry as cce join composition on cce.composition_id = composition.id join concordance on cce.concordance_id = concordance.id where cce.concordance_index=0
+                                UNION
+                                SELECT NULL as concordance_man, concordance.shorthand as concordance_print, NULL as concordance_chr, composition_id
+                                from composition_concordance_entry as cce join composition on cce.composition_id = composition.id join concordance on cce.concordance_id = concordance.id where cce.concordance_index=1
+                                UNION
+                                SELECT NULL as concordance_man, NULL as concordance_print, concordance.shorthand as concordance_chr, composition_id
+                                from composition_concordance_entry as cce join composition on cce.composition_id = composition.id join concordance on cce.concordance_id = concordance.id where cce.concordance_index=2
+                                ;
+                                ''')
+                    cur.execute('''CREATE VIEW concordance_composition_joined_view as
+                    SELECT
+                    string_agg(concordance_man,', ') as concordance_man, string_agg(concordance_print, ', ') as concordance_print, 
+                    string_agg(concordance_chr, ', ') as concordance_chr, composition_id from concordance_composition_view group by composition_id
+                    ;
+                    ''')
+                    cur.execute('''CREATE VIEW all_joined_view as
+                                                     SELECT composer_src, composer_rism, composer_chr,
+                                                     track_no, title, remark, mode, printed_in, genre, scribe, 
+                                                     concomv.concordance_man, concomv.concordance_print, concomv.concordance_chr
+                                                     FROM
+                                                     concordance_composition_joined_view concomv join composition_composer_view comcomv 
+                                                     ON 
+                                                     concomv.composition_id = comcomv.composition_id 
+                                                     ;
+                                                     ''')
+
+                    cur.execute('''CREATE VIEW all_view as
+                                  SELECT composer_src, composer_rism, composer_chr,
+                                  track_no, title, remark, mode, printed_in, genre, scribe, 
+                                  concomv.concordance_man, concomv.concordance_print, concomv.concordance_chr
+                                  FROM
+                                  concordance_composition_view concomv join composition_composer_view comcomv 
+                                  ON 
+                                  concomv.composition_id = comcomv.composition_id 
+                                  ;
+                                  ''')
 
 
 def getId(conn, table, col, name):
@@ -83,11 +126,24 @@ def insert_song(conn, track_no, title, remark, mode, printed_in, genre, scribe):
 
 def insert_song_composers(conn, composition_id, comp_src_id, comp_rism_id, comp_chr_id):
     id = None
-    #print(composition_id, comp_src_id, comp_rism_id, comp_chr_id)
     with conn.cursor() as cursor:
         cursor.execute("INSERT INTO composition_composer ({}) VALUES(%s,%s,%s,%s)".
                        format("composition, composer_src, composer_rism, composer_chr"),
                        (composition_id, comp_src_id, comp_rism_id, comp_chr_id))
+
+def insert_composition_concordance(conn, composition_id, cond_man, cond_print, cond_chr):
+    with conn.cursor() as cursor:
+        for index, cond_list in enumerate([cond_man, cond_print, cond_chr]):
+            for cond in cond_list.split(','):
+                if cond == "":
+                    continue
+                cond_id = getId(conn, "concordance", "shorthand", cond.strip())
+                cursor.execute("INSERT INTO composition_concordance_entry ({}) VALUES(%s,%s,%s)".
+                               format("concordance_id, composition_id, concordance_index"),
+                               (cond_id, composition_id, index))
+
+
+
 
 def entry_is_sane(entry):
     [composer_src, composer_rism, composer_chr, track_no, title, remark, mode, printed_in, genre, scribe, cond_man,
@@ -138,3 +194,4 @@ def parse_data(input_file: object) -> object:
                 # print(comp_chr_id)
                 composition_id = insert_song(con, track_no, title, remark, mode, printed_in, genre, scribe)
                 insert_song_composers(con, composition_id, comp_src_id, comp_rism_id, comp_chr_id)
+                insert_composition_concordance(con, composition_id, cond_man, cond_print, cond_chr)
